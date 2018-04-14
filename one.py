df62ee2f3b738eeb0dc37fe5c9b8f4b5f116c762
张炜奇 <zwq1015@163.com>
2018-04-14 13:39:50 +0800

    hello pygame

--- one.py
+++ one.py
@@ -7,8 +7,15 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+
+myfont = pygame.font.Font(None, 60)
+
+white = 255, 255, 255
+blue = 0, 0, 200
+
+textImage = myfont.render("Hello Pygame", True, white)
 screen = pygame.display.set_mode((600, 500))
-pygame.display.set_caption("Drawing Rectangles")
+# pygame.display.set_caption("Drawing Rectangles")
 
 pos_x = 300
 pos_y = 250
@@ -18,5 +25,9 @@ vel_y = 1
 while True:
     for event in pygame.event.get():
         if event.type in (QUIT, KEYDOWN):
-            pygame.quit()
+            # pygame.quit()
             sys.exit()
+
+    screen.fill(blue)
+    screen.blit(textImage, (100, 100))
+    pygame.display.update()
